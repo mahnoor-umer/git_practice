688a09d2301fe48d372594649db3d1038f58f919
mahnoor_umer <mahnoorumer10@gmail.com>
2025-06-19 11:47:08 +0500

    modifying the app

--- tests/test.py
+++ tests/test.py
@@ -6,9 +6,11 @@ def test_add():
     assert add(-5, 3) == -2
     assert add(-5, -3) == -8
     assert add(5, -3) == 2
+    assert add(0, 0) == 0
 
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(-5, 3) == -8
     assert subtract(-5, -3) == -2
     assert subtract(5, -3) == 8
+    assert subtract(0, 0) == 0
